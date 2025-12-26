de669e78d698e3bce5f31689f17559e20b5edcdd
CARLOS FERNANDO LEAL OLIVA <paintsoliva@gmail.com>
2025-12-26 17:34:30 -0500

    micro properties updated: imports fixed

---
+++ micro-properties/src/main/java/com/micro/properties/micro_properties/controller/PropertyController.java
@@ -0,0 +1,40 @@
+package com.micro.properties.controller;
+
+import com.micro.properties.model.Property;
+import com.micro.properties.service.PropertyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/properties") // Prefijo que configuramos en el Gateway
+public class PropertyController {
+
+    @Autowired
+    private PropertyService propertyService;
+
+    // POST: Crear una casa
+    @PostMapping
+    public ResponseEntity<Property> createProperty(@RequestBody Property property) {
+        return new ResponseEntity<>(propertyService.createProperty(property), HttpStatus.CREATED);
+    }
+
+    // GET: Listar todas
+    @GetMapping
+    public ResponseEntity<List<Property>> getAllProperties() {
+        return ResponseEntity.ok(propertyService.getAllProperties());
+    }
+
+    // GET: Obtener una por ID
+    @GetMapping("/{id}")
+    public ResponseEntity<Property> getPropertyById(@PathVariable Long id) {
+        Property property = propertyService.getPropertyById(id);
+        if (property == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(property);
+    }
+}
---
+++ micro-properties/src/main/java/com/micro/properties/micro_properties/repository/PropertyRepository.java
@@ -0,0 +1,11 @@
+package com.micro.properties.repository;
+
+import com.micro.properties.model.Property;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PropertyRepository extends JpaRepository<Property, Long> {
+    // Aquí podrías agregar métodos mágicos como:
+    // List<Property> findByBedroomsGreaterThan(int count);
+}
